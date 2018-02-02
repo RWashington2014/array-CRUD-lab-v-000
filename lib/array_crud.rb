20d4abed1330b225aa9ae581038e8d348dbf1410
Robert W <robwash@gmail.com>
2018-02-02 20:12:27 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,10 @@
 def create_an_empty_array
   Array.new
+  #[]
 end
 
 def create_an_array
-
+  ["Optimus Prime", "Megatron", "BumbleBee", "HotRod"]
 end
 
 def add_element_to_end_of_array(array, element)
